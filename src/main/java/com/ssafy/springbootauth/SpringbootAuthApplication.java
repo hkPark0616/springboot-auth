6cc6fdaed7c7b179482403f5095e35b97a08305e
qkrgorn3617 <qkrgorn3617@naver.com>
2025-01-06 16:28:02 +0900

    init

---
+++ src/main/java/com/ssafy/springbootauth/SpringbootAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.springbootauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAuthApplication.class, args);
+    }
+
+}
